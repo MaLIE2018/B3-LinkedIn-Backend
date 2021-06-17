430d38898ac984ddf2e2ade2a0808a037b74aab6
MaLIE2018 <liebsch@pm.me>
2021-06-17 18:38:54 +0200

    withoutcsv

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -4,7 +4,7 @@ import { CloudinaryStorage } from "multer-storage-cloudinary"
 import multer from "multer"
 import models from "../utils/db/index.js"
 import createError from "http-errors"
-import  {Parser, Transform } from "json2csv"
+// import  {Parser, Transform } from "json2csv"
 import { pipeline, Readable } from "stream"
 
 const fr = express.Router()
@@ -94,27 +94,27 @@ fr.post("/:postId/uploadPostImage", uploadPostImage, async (req, res, next) => {
 
 // Export experiences as CSV
 
-fr.get("/:profileId/exportCSV", async (req, res, next) => {
-  try {
-    const userExperiences = await Experience.findAll({
-      where: { profileId: req.params.profileId },
-    })
+// fr.get("/:profileId/exportCSV", async (req, res, next) => {
+//   try {
+//     const userExperiences = await Experience.findAll({
+//       where: { profileId: req.params.profileId },
+//     })
 
-    const fields = ["id", "role", "company", "startDate", "endDate", "description", "area"]
+//     const fields = ["id", "role", "company", "startDate", "endDate", "description", "area"]
 
-    const parser = new Parser({ fields })
-    const csv = parser.parse(userExperiences)
-    const source = Readable.from(csv)
+//     const parser = new Parser({ fields })
+//     const csv = parser.parse(userExperiences)
+//     const source = Readable.from(csv)
 
-    res.setHeader("Content-Disposition", `attachment; filename=experiences.csv`)
+//     res.setHeader("Content-Disposition", `attachment; filename=experiences.csv`)
 
-    pipeline(source, res, (error) => {
-      if (error) next(error)
-    })
-  } catch (error) {
-    next(error)
-  }
+//     pipeline(source, res, (error) => {
+//       if (error) next(error)
+//     })
+//   } catch (error) {
+//     next(error)
+//   }
 
-})
+// })
 
 export default fr
